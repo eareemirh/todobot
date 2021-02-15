9a7bf2f068c5e47864368083b4e50d96c48c68a6
Eare <emirhan1970@protonmail.com>
2021-02-15 03:32:27 +0300

    Create Logger.js

---
+++ src/Base/Modules/Logger.js
@@ -0,0 +1,40 @@
+const moment = require("moment");
+const chalk = require("chalk");
+
+class Logger {
+  log(content, type = "LOG") {
+    let now = moment().format("LLLL");
+    let ltype = type.toLowerCase();
+    type = type.toUpperCase();
+    switch (ltype) {
+      case "log":
+        return console.log(
+          `${now} | ${chalk.black.bgBlue(`<${type}>`)} ${content}`
+        );
+      case "warn":
+        return console.log(
+          `${now} | ${chalk.black.bgYellow(`<${type}>`)} ${content}`
+        );
+      case "error":
+        return console.log(
+          `${now} | ${chalk.black.bgRed(`<${type}>`)} ${content}`
+        );
+      case "debug":
+        return console.log(
+          `${now} | ${chalk.black.bgMagenta(`<${type}>`)} ${content}`
+        );
+      case "cmd":
+        return console.log(
+          `${now} | ${chalk.black.bgWhite(`<${type}>`)} ${content}`
+        );
+      case "ready":
+        return console.log(
+          `${now} | ${chalk.black.bgGreen(`<${type}>`)} ${content}`
+        );
+      default:
+        throw new Error("Logger boş olamaz.");
+    }
+  }
+}
+
+module.exports = Logger;
